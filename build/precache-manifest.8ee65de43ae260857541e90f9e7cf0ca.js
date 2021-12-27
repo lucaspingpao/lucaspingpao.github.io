138f591219ee7edeb6cdbf5c363f313455188f2d
Lucas <lucaspingpao@gmail.com>
2021-12-27 21:36:41 +0100

    test

---
+++ build/precache-manifest.8ee65de43ae260857541e90f9e7cf0ca.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c412a0f1fdebbd36d2c132455d1ee38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c505b8fdddf50b2a081",
+    "url": "/static/css/main.48d32cae.chunk.css"
+  },
+  {
+    "revision": "f3db28ca2090133e0499",
+    "url": "/static/js/2.fc799885.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fc799885.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c505b8fdddf50b2a081",
+    "url": "/static/js/main.f4918b34.chunk.js"
+  },
+  {
+    "revision": "085495857938a6ab9c24",
+    "url": "/static/js/runtime-main.09c80ff0.js"
+  },
+  {
+    "revision": "a3f9d5eba273c8f6699997bf2042a2f3",
+    "url": "/static/media/Composition.a3f9d5eb.jpg"
+  },
+  {
+    "revision": "51a21dcd754fdd5bc80934c9c8cee0df",
+    "url": "/static/media/DJing.51a21dcd.jpg"
+  },
+  {
+    "revision": "cefc20232703e5e3c24efd5f50d75e26",
+    "url": "/static/media/GitHubLogo.cefc2023.png"
+  },
+  {
+    "revision": "44f916c5c0482a76606001a05d984f76",
+    "url": "/static/media/JustMoveRightAlong.44f916c5.png"
+  },
+  {
+    "revision": "290e8a74b4a2f4b735380fdc56611815",
+    "url": "/static/media/LeaveYouAlone.290e8a74.png"
+  },
+  {
+    "revision": "a77203db8fa7c9b7817c82f4dab5f650",
+    "url": "/static/media/OneMoveAtATime.a77203db.png"
+  },
+  {
+    "revision": "2197421418b2e7580c2e0626ba5470d3",
+    "url": "/static/media/Spotify.svg.21974214.png"
+  },
+  {
+    "revision": "8639167af76d0e39b4f358f041af4061",
+    "url": "/static/media/WhereDidYouGo.8639167a.JPG"
+  },
+  {
+    "revision": "4b4c46a8e680818f3e8fb12e3d7c99d9",
+    "url": "/static/media/amazonMusic.4b4c46a8.png"
+  },
+  {
+    "revision": "dc2f332484412c04505bf4b76828b523",
+    "url": "/static/media/hundred.dc2f3324.png"
+  },
+  {
+    "revision": "ac59ff33247deea1e8710906aab5556f",
+    "url": "/static/media/iTunesStore.svg.ac59ff33.png"
+  },
+  {
+    "revision": "287a8a8aeb472bf466e3436581f000f5",
+    "url": "/static/media/ivyhacks.287a8a8a.png"
+  },
+  {
+    "revision": "b135deb02cf44ba14d59642f002b34de",
+    "url": "/static/media/math.b135deb0.jpg"
+  },
+  {
+    "revision": "fbe86dfc5cea6696c3ae54412b5fbbde",
+    "url": "/static/media/schedule.fbe86dfc.png"
+  },
+  {
+    "revision": "b6b02521ba2c33cb88f0b209ef624900",
+    "url": "/static/media/serendipity.b6b02521.mp3"
+  },
+  {
+    "revision": "07b8560a6a6eb7155b80b0c599e6874e",
+    "url": "/static/media/youtube.07b8560a.png"
+  }
+]);
